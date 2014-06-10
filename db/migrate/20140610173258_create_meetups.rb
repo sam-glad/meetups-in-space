820936263c55bdb1c2eb139e24c75b82f1e90c46
Sam Gladstone <gladston@umich.edu>
2014-06-10 13:40:36 -0400

    Meetups now included in database

---
+++ db/migrate/20140610173258_create_meetups.rb
@@ -0,0 +1,9 @@
+class CreateMeetups < ActiveRecord::Migration
+  def change
+    create_table :meetups do |t|
+      t.string :name, null: false
+      t.text :description
+      t.string :location, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606200404) do
+ActiveRecord::Schema.define(version: 20140610173258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetups", force: true do |t|
+    t.string "name",        null: false
+    t.text   "description"
+    t.string "location",    null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider",   null: false
